91ee40c735cb41793b867b547782e2ead980dd99
JhonTreft <jonz37133@gmail.com>
2023-04-14 14:13:41 -0500

    config project

---
+++ bin/manage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+if __name__ == "__main__":
+    # Agregar la ruta del proyecto a sys.path
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    sys.path.insert(0, project_root)
+
+    # Establecer la variable de entorno DJANGO_SETTINGS_MODULE
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "back_core.settings")
+
+    # Ejecutar el comando de gestión de Django
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
