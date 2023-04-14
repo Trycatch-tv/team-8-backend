63375750b10db6d7c9ab6079e2869d5351a6c993
JhonTreft <jonz37133@gmail.com>
2023-04-14 16:15:10 -0500

    modification to wrap it in the apps folder

--- apps/administration_sc/apps.py
+++ apps/administration_sc/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AdministrationScConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'administration_sc'
+    name = 'apps.administration_sc'
