6c9a6e0b8289316128ba403d6750d884dc8bfb2a
JhonTreft <jonz37133@gmail.com>
2023-04-17 17:28:40 -0500

    change models

--- apps/administration_sc/models.py
+++ apps/administration_sc/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
 
-# Create your models here.
+class estudianteModel(serializers.Serializers):
+    nombre = models.CharField(max_length=60)
+    correo = models.EmailField(max_length=60)
+    
+
+
+class profesorModel(serializers.Serializers):
+    pass
+
+class cursoModel(serializers.Serializers):
+    #name= models.CharField(max_length=90)
+    #created_at = models.DateTimeField(auto_now=True)
+    #valoraciones = models.ForeignKey(estudianteModel)
+    
+    
+    
+    pass
