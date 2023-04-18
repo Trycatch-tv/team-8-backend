cb291387586a0912f4c14b43d8e44938a692e7aa
JhonTreft <jonz37133@gmail.com>
2023-04-18 16:17:42 -0500

    add views the models

--- apps/administration_sc/views.py
+++ apps/administration_sc/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from .serializer import *
+from .models import *
 
 # Create your views here.
+
+
+
+class people_list(generics.ListCreateAPIView):
+    queryset = Person.objects.all()
+    serializer_class= PersonSerializer
+
+class studentlist():
+    queryset = estudianteModel.objects.all()
+    serializer_class = studentSerializer
