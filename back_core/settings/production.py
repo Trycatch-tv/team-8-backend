6beb06c07764806f2ec4e230e908fb4dfe13b7c4
JhonTreft <jonz37133@gmail.com>
2023-04-18 17:04:21 -0500

    add config production db

--- back_core/settings/production.py
+++ back_core/settings/production.py
@@ -2,9 +2,11 @@
 DEBUG = False
 
 #Database Seria Postgresql
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
