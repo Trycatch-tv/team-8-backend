cb64736e4da56895562522f020ae8924aa383160
JhonTreft <jonz37133@gmail.com>
2023-04-18 16:16:11 -0500

    add models in the admin

--- apps/administration_sc/admin.py
+++ apps/administration_sc/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+@admin.register(estudianteModel)
+class studenAdmin(admin.ModelAdmin):
+    pass
+@admin.register(profesorModel)
+class teacherAdmin(admin.ModelAdmin):
+    pass
+@admin.register(cursoModel)
+class cursoAdmin(admin.ModelAdmin):
+    pass
+    
+
+
+
+    
+    
+
+
