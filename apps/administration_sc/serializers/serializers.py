f6d994db5dcc6c01b17405001d41df51015ea648
JhonTreft <jonz37133@gmail.com>
2023-04-18 16:35:25 -0500

    add serializers for models

---
+++ apps/administration_sc/serializers/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from ..models import *
+
+class student_serializers(serializers.ModelSerializer):
+    class Meta:
+        model = estudianteModel
+        fields = '__all__'
+        
+class teacher_serializers(serializers.ModelSerializer):
+    class Meta:
+        model = profesorModel
+        fields = '__all__'
+class course_serializers(serializers.ModelSerializer):
+    class Meta:
+        model = cursoModel
+        fields = '__all__'
+        
