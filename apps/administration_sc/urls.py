ac09093837b71250b44f6adab72c4285da013417
JhonTreft <jonz37133@gmail.com>
2023-04-18 16:49:34 -0500

    add urls of the views

--- apps/administration_sc/urls.py
+++ apps/administration_sc/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-        path('list_people/',people_list.as_view(),name="list-people"),
+        path('list_students/',student_list.as_view(),name="students-people"),
+        path('list_courses/',course_list.as_view(),name="courses-people"),
+        path('list_teachers/',teacher_list.as_view(),name="teachers-people"),
 ]
