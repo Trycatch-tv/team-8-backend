d669cc5fddc6ea58201455d66d2f9aae7a836aef
JhonTreft <jonz37133@gmail.com>
2023-04-18 17:58:09 -0500

    add views of delete and put

--- apps/administration_sc/views.py
+++ apps/administration_sc/views.py
@@ -1,19 +1,54 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.generics import DestroyAPIView,UpdateAPIView,RetrieveUpdateAPIView,ListCreateAPIView
 from rest_framework.decorators import api_view
 from .serializers.serializers import *
 from .models import *
 
 # Create your views here.
 
-class student_list(generics.ListCreateAPIView):
+
+# ADD ACTIONS
+class student_list(ListCreateAPIView):
     queryset = estudianteModel.objects.all()
     serializer_class = student_serializers
 
-class teacher_list(generics.ListCreateAPIView):
+class teacher_list(ListCreateAPIView):
     queryset = profesorModel.objects.all()
     serializer_class = teacher_serializers
     
-class course_list(generics.ListCreateAPIView):
+class course_list(ListCreateAPIView):
+    queryset = cursoModel.objects.all()
+    serializer_class = course_serializers
+
+
+
+
+# UPDATE ACTIONS
+class update_student(RetrieveUpdateAPIView):
+    queryset = estudianteModel.objects.all()
+    serializer_class = student_serializers
+    
+    
+class update_course(RetrieveUpdateAPIView):
+    queryset = cursoModel.objects.all()
+    serializer_class = course_serializers
+    
+class update_teacher(RetrieveUpdateAPIView):
+    queryset = profesorModel.objects.all()
+    serializer_class = teacher_serializers
+    
+    
+#DELETE ACTIONS
+    
+class delete_student(DestroyAPIView):
+    queryset = estudianteModel.objects.all()
+    serializer_class = student_serializers
+    
+    
+class delete_teacher(UpdateAPIView):
+    queryset = profesorModel.objects.all()
+    serializer_class = teacher_serializers
+    
+class delete_course(UpdateAPIView):
     queryset = cursoModel.objects.all()
     serializer_class = course_serializers
