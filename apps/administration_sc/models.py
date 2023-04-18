05222218481067de73bc229edc71169761de6cda
JhonTreft <jonz37133@gmail.com>
2023-04-18 16:16:49 -0500

    change models curso and student and teacher add choices models

--- apps/administration_sc/models.py
+++ apps/administration_sc/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
-from rest_framework import serializers
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
+
+
+class categoria_choices(models.TextChoices):
+    OPCION_1 = 'Academico', _('Academico')
+    OPCION_2 = 'Tecnologico', _('Tecnologico')
+
+class nivel_choices(models.TextChoices):
+    OPCION_1 = 'Basico', _('Basico')
+    OPCION_2 = 'Intermedio', _('Intermedio')
+    OPCION_3 = 'Avanzado' ,('Avanzado')
+
 
 class ciudad_choices(models.TextChoices):
     OPCION_1 = 'Colombia', _('Colombia')
@@ -16,36 +26,60 @@ class ciudad_choices(models.TextChoices):
     OPCION_10 = 'Chile', _('Chile')
     OPCION_11 = 'Panama', _('Panama')
 
-class estudianteModel(serializers.Serializers):
-    nombre = models.CharField(max_length=60)
-    correo = models.EmailField(max_length=60)
-    ciudad = models.TextChoices(choice=ciudad_choices)
-    telefono = models.IntegerField()
-    estado = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now=True)
-    id_curso = models.ForeignKey(cursoModel, on_delete=models.CASCADE)
+class cursoModel(models.Model):
+    name = models.CharField(max_length=90)
+    categoria = models.CharField(
+        choices=categoria_choices.choices,
+        default=categoria_choices.OPCION_1,
+        max_length=11,
+        blank=True
+    )
+    nivel_curso = models.CharField(
+        choices=nivel_choices.choices,
+        default=nivel_choices.OPCION_1,
+        max_length=11,
+                blank=True
+
+    )
+    numero_max_estudiantes = models.IntegerField(blank=True, null=True)
+    codigo_curso = models.CharField(max_length=30,blank=True)
+    created_at = models.DateField(auto_now=True)
+    valoraciones = models.ForeignKey('estudianteModel', on_delete=models.CASCADE, related_name='curso_estudent_vc')
+    id_estudiante = models.ForeignKey('estudianteModel', on_delete=models.CASCADE , related_name='curso_estudent')
+    id_profesor = models.ForeignKey('profesorModel', on_delete=models.CASCADE, related_name='curso_teacher')     
+    estado = models.CharField(max_length=15)
+    
     
     def __str__(self) -> str:
         return self.nombre
 
-class profesorModel(serializers.Serializers):
+class profesorModel(models.Model):
     nombre = models.CharField(max_length=90)
     correo = models.EmailField(max_length=60)
     telefono = models.IntegerField()
     descripcion = models.TextField()
     area_especializacion = models.CharField(max_length=50)
-    id_estudiante = models.ForeignKey(estudianteModel)
-    id_curso = models.ForeignKey(cursoModel)
+    id_estudiante = models.ForeignKey('estudianteModel', on_delete=models.CASCADE, related_name='estudent_teacher')
+    id_curso = models.ForeignKey(cursoModel, on_delete=models.CASCADE, related_name='curso_teacher')
     created_at = models.DateTimeField(auto_now=True)
     estado = models.CharField(max_length=15)
     
-
-class cursoModel(serializers.Serializers):
-    name = models.CharField(max_length=90)
-    creted_at = models.DateField(auto_now=True)
-    valoraciones = models.ForeignKey(estudianteModel)
-    id_estudiante = models.ForeignKey(estudianteModel)
-    id_profesor = models.ForeignKey(profesorModel)     
-    estado = models.CharField(max_length=15)
     
+    def __str__(self) -> str:
+        return self.nombre
 
+class estudianteModel(models.Model):
+    nombre = models.CharField(max_length=60)
+    correo = models.EmailField(max_length=60)
+    ciudad = models.CharField(choices=ciudad_choices.choices,
+                              default=ciudad_choices.OPCION_1,
+                              max_length=14,
+                              )
+    telefono = models.IntegerField()
+    estado = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now=True)
+    id_curso = models.ForeignKey(cursoModel, on_delete=models.CASCADE , related_name='estudent_curso')
+    id_profesor = models.ForeignKey(profesorModel, on_delete=models.CASCADE, related_name='estudiante_teacher')
+    
+    def __str__(self) -> str:
+        return self.nombre
