f64db6350fb307fe119de2cda59c01adb5a74fe1
JhonTreft <jonz37133@gmail.com>
2023-04-18 16:50:06 -0500

    change instance ApiCreate in the class

--- apps/administration_sc/views.py
+++ apps/administration_sc/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.decorators import api_view
-from .serializer import *
+from .serializers.serializers import *
 from .models import *
 
 # Create your views here.
 
-class student_list():
+class student_list(generics.ListCreateAPIView):
     queryset = estudianteModel.objects.all()
     serializer_class = student_serializers
 
-class teacher_list():
+class teacher_list(generics.ListCreateAPIView):
     queryset = profesorModel.objects.all()
     serializer_class = teacher_serializers
     
-class course_list():
+class course_list(generics.ListCreateAPIView):
     queryset = cursoModel.objects.all()
     serializer_class = course_serializers
