d3c2e58a46e1c96c93fb618c31ae516b562924ab
JhonTreft <jonz37133@gmail.com>
2023-04-22 18:00:43 -0500

    add urls of login

--- apps/administration_sc/urls.py
+++ apps/administration_sc/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
+        #add Urls Actions
         path('add_student/', add_student.as_view(), name='add_student'),
         path('add_teacher/', add_teacher.as_view(), name='add_teacher'),
         path('add_course/', add_course.as_view(), name='add_course'),
-
         #List Urls Actions
         path('list_students/',student_list.as_view(),name="students_people"),
         path('list_courses/',course_list.as_view(),name="courses_people"),
@@ -19,4 +19,10 @@ urlpatterns=[
         path('delete_student/<int:pk>/', delete_student.as_view()),
         path('delete_teacher/<int:pk>/', delete_teacher.as_view()),
         path('delete_course/<int:pk>/', delete_course.as_view()),
+        
+        #Login 
+        path('user-login/', UserLoginAPIView.as_view(), name='user_login_api'),
+        path('student-login/', StudentLoginView.as_view(), name='student_login_api'),
+        path('teacher-login/', TeacherLoginView.as_view(), name='teacher_login_api'),
+
 ]
