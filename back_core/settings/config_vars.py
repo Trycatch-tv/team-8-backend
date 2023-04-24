0feb3fbb9e7a7fd4e313b277e5b960ef04f71144
JhonTreft <jonz37133@gmail.com>
2023-04-24 09:41:00 -0500

    add cors permission

--- back_core/settings/config_vars.py
+++ back_core/settings/config_vars.py
@@ -28,13 +28,15 @@ WSGI_APPLICATION = 'back_core.wsgi.application'
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Lima'
 
 USE_I18N = True
 
 USE_TZ = True
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Static files (CSS, JavaScript, Images)
