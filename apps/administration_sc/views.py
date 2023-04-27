4ee12cc5e22a16e116a0cf424cb648dc1a67302b
JhonTreft <jonz37133@gmail.com>
2023-04-27 09:57:10 -0500

    add function get in the views update in the student , teacher, course

--- apps/administration_sc/views.py
+++ apps/administration_sc/views.py
@@ -38,12 +38,52 @@ class update_student(RetrieveUpdateAPIView):
     queryset = estudianteModel.objects.all()
     serializer_class = student_serializers
     
+    def get(self, request, *args, **kwargs):
+        print("pass")
+        # Obtiene el ID del estudiante de los parámetros de la solicitud
+        id_estudiante = kwargs.get('pk')
+        # Consulta el objeto estudiante con el ID especificado
+        estudiante = self.queryset.filter(id=id_estudiante).first()
+        # Verifica si el objeto existe
+        if not estudiante:
+            return Response({"error": "El estudiante no existe"}, status=status.HTTP_404_NOT_FOUND)
+        # Serializa el objeto estudiante y devuelve los datos
+        serializer = self.serializer_class(estudiante)
+        return Response(serializer.data)
+    
 class update_course(RetrieveUpdateAPIView):
     queryset = cursoModel.objects.all()
     serializer_class = course_serializers
+    
+    def get(self, request, *args, **kwargs):
+        print("pass")
+        # Obtiene el ID del estudiante de los parámetros de la solicitud
+        id_course = kwargs.get('pk')
+        # Consulta el objeto estudiante con el ID especificado
+        course = self.queryset.filter(id=id_course).first()
+        # Verifica si el objeto existe
+        if not course:
+            return Response({"error": "El course no existe"}, status=status.HTTP_404_NOT_FOUND)
+        # Serializa el objeto estudiante y devuelve los datos
+        serializer = self.serializer_class(course)
+        return Response(serializer.data)
 class update_teacher(RetrieveUpdateAPIView):
     queryset = profesorModel.objects.all()
     serializer_class = teacher_serializers
+    
+    def get(self, request, *args, **kwargs):
+        print("pass")
+        # Obtiene el ID del estudiante de los parámetros de la solicitud
+        id_teacher = kwargs.get('pk')
+        # Consulta el objeto estudiante con el ID especificado
+        teacher = self.queryset.filter(id=id_teacher).first()
+        # Verifica si el objeto existe
+        if not teacher:
+            return Response({"error": "El teacher no existe"}, status=status.HTTP_404_NOT_FOUND)
+        # Serializa el objeto estudiante y devuelve los datos
+        serializer = self.serializer_class(teacher)
+        return Response(serializer.data)
+    
     
 #DELETE ACTIONS
     
