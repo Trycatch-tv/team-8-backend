52e6a90fa44b6e3744274e4a7c566d7af9803f87
JhonTreft <jonz37133@gmail.com>
2023-04-28 18:08:39 -0500

    add urls relations

--- apps/administration_sc/urls.py
+++ apps/administration_sc/urls.py
@@ -23,9 +23,24 @@ urlpatterns=[
         path('user-login/', UserLoginAPIView.as_view(), name='user_login_api'),
         path('student-login/', StudentLoginView.as_view(), name='student_login_api'),
         path('teacher-login/', TeacherLoginView.as_view(), name='teacher_login_api'),
+        
+        #Relations
         path('profesores/<int:id>/estudiantes/', TeacherStudent.as_view(), name='profesor-estudiantes'),
 
+        #get relations
         #path('teacher-student/<int:pk>/', TeacherStudent.as_view(),name="teacher_student"),
-    path('api/estudiantes/<int:estudiante_id>/cursos/', CursosEstudianteView.as_view(), name='cursos_estudiante'),        path('agregar_curso_profesor_a_estudiante/', agregar_curso_profesor_a_estudiante, name='agregar_curso_profesor_a_estudiante'),
+        path('api/estudiantes/<int:estudiante_id>/cursos/', CursosEstudianteView.as_view(), name='cursos_estudiante'),        
+        path('agregar_curso_profesor_a_estudiante/', agregar_curso_profesor_a_estudiante, name='agregar_curso_profesor_a_estudiante'),
+
+
+        path('agregar_curso_profesor/', agregar_curso_profesor, name='agregar_curso_profesor_a_estudiante'),
+        path('api/profesor/<int:profesor_id>/cursos/', ProfesorCursosView.as_view(), name='cursos_profesores'),   
+        
+        
+        path('api/profesores/<int:profesor_id>/estudiantes/', ProfesorEstudiantes.as_view(), name='estudiantes_profesores'),   
+        
+        
+        
+        
             
 ]
